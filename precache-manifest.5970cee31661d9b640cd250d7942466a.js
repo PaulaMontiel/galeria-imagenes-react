eb46d27639c6496b440fa5a0f0a1bc5101d9f20a
PaulaMontiel <p_dany_mr@hotmail.com>
2022-09-06 22:35:59 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.5970cee31661d9b640cd250d7942466a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f9531ad52917c15f44b",
+    "url": "/PaulaMontiel/galeria-imagenes-react/static/css/main.90eac20d.chunk.css"
+  },
+  {
+    "revision": "5f9531ad52917c15f44b",
+    "url": "/PaulaMontiel/galeria-imagenes-react/static/js/main.5f9531ad.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/PaulaMontiel/galeria-imagenes-react/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "b263cfdc4621885e3687",
+    "url": "/PaulaMontiel/galeria-imagenes-react/static/css/2.b01ccba5.chunk.css"
+  },
+  {
+    "revision": "b263cfdc4621885e3687",
+    "url": "/PaulaMontiel/galeria-imagenes-react/static/js/2.b263cfdc.chunk.js"
+  },
+  {
+    "revision": "26f16377cf18622e0782",
+    "url": "/PaulaMontiel/galeria-imagenes-react/static/js/runtime~main.26f16377.js"
+  },
+  {
+    "revision": "caec2e79f646268cbcc6652bf8c3e1b2",
+    "url": "/PaulaMontiel/galeria-imagenes-react/index.html"
+  }
+];
